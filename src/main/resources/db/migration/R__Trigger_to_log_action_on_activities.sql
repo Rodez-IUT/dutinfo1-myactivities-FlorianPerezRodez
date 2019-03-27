d6586b9a763f5bbb602424cb6c5f4ef688a0fefa
Florian Perez <florian.perez@iut-rodez.fr>
2019-03-27 15:39:46 +0100

    Fix 5.2 trigger to log action on activitie fini

--- src/main/resources/db/migration/R__Trigger_to_log_action_on_activities.sql
+++ src/main/resources/db/migration/R__Trigger_to_log_action_on_activities.sql
@@ -1,5 +1,14 @@
-CREATE TRIGGER check_delete
-    BEFORE DELETE ON activity
+DROP TRIGGER IF EXISTS check_delete ON activity;
+
+CREATE OR REPLACE FUNCTION insert_action_log() RETURNS trigger AS $$
+		DECLARE
+		BEGIN
+			INSERT INTO action_log VALUES(nextval('id_generator'),'delete','activity','postgres',OLD.id);
+			RETURN NULL;
+		END
+$$LANGUAGE plpgsql;
+
+CREATE TRIGGER check_delete 
+    AFTER DELETE ON activity
     FOR EACH ROW
-    EXECUTE PROCEDURE log_account_update();
-$$ LANGUAGE SQL;
+    EXECUTE PROCEDURE insert_action_log();
